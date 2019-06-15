a1ced8a209c4d469541c21efaed80bc95eeabab3
Carlos Santos <carloseliesersantos@gmail.com>
2019-06-15 03:57:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -114,7 +114,7 @@ function createWindow() {
 
     mainWindow.loadFile(path.join(__dirname, 'index.html'))
 
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
