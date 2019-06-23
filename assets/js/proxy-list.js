4339311334246f37949e067ab14f1600f2704115
Carlos Santos <carloseliesersantos@gmail.com>
2019-06-22 20:43:07 -0400

    Set timeout on fetching proxys

--- assets/js/proxy-list.js
+++ assets/js/proxy-list.js
@@ -1,42 +1,38 @@
-let ProxyList = function() {
-	let request = require("axios");
+let ProxyList = function () {
+	let axios = require("axios");
+	let request = axios.create({
+		timeout: 4000
+	});
 	let url = "https://proxy.rudnkh.me/txt";
 
 	let getProxies = (text) => {
-		return new Promise((resolve, reject) => {
-			let proxies = text.split("\n");
-			for (let i = 0; i < proxies.length; i++) {
-				let info = proxies[i].split(":");
-				let proxy = {
-					host: info[0],
-					port: info[1]
-				};
+		let proxies = text.split("\n");
+		for (let i = 0; i < proxies.length; i++) {
+			let info = proxies[i].split(":");
+			let proxy = {
+				host: info[0],
+				port: info[1]
+			};
 
-				proxies[i] = proxy;
-			}
+			proxies[i] = proxy;
+		}
 
-			resolve(proxies);
-		}).catch((err) => {
-			console.log(err);
-		});
+		return proxies;
 	};
 
 	let listProxies = () => {
 		return new Promise((resolve, reject) => {
 			request.get(url).then((response) => {
 				let text = response.data;
-				getProxies(text)
-					.then((proxies) => {
-						resolve(proxies);
-					})
-					.catch((err) => reject(err));
-			}).catch((err) => reject(err));
-		}).catch((err) => {
-			console.log(err);
-		});
+				let proxies = getProxies(text);
+				resolve(proxies);
+			}).catch((err) => resolve(err));
+		})
 	};
 
-	return { listProxies };
+	return {
+		listProxies
+	};
 };
 
 export default ProxyList;
