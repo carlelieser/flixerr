164a384173db9463388068fea45b1073df0539c0
Carlos Santos <carloseliesersantos@gmail.com>
2019-06-22 20:33:39 -0400

    Update saved movie time on reload

--- assets/js/backup-torrent-container.jsx
+++ assets/js/backup-torrent-container.jsx
@@ -1,81 +1,86 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import uniqid from "uniqid";
 
 import Torrent from "./torrent";
 
 class BackupTorrents extends Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	handleReload = () => {
-		this.props.setPlayerLoading(true);
-		this.props
-			.resetClient(true)
-			.then((result) => {
-				let movie = this.props.movie;
-				movie.magnet = false;
-				this.props.searchTorrent(movie);
-			})
-			.catch((err) => console.log(err));
-		this.props.closeBackup();
-	};
+    handleReload = () => {
+        this
+            .props
+            .setPlayerLoading(true);
+        this
+            .props
+            .updateMovieTime(this.props.currentTime);
+        this
+            .props
+            .resetClient(true)
+            .then((result) => {
+                let movie = this.props.movie;
+                movie.magnet = false;
+                this
+                    .props
+                    .searchTorrent(movie);
+            })
+            .catch((err) => console.log(err));
+        this
+            .props
+            .closeBackup();
+    };
 
-	shouldComponentUpdate(nextProps, nextState) {
-		if (
-			nextProps.torrents !== this.props.torrents ||
-			nextProps.movie.magnet !== this.props.movie.magnet
-		) {
-			return true;
-		}
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.torrents !== this.props.torrents || nextProps.movie.magnet !== this.props.movie.magnet) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	render() {
-		let torrents = this.props.torrents
-			? this.props.torrents.map((torrent) => {
-					let title = torrent.title;
-					let videoQuality = torrent.resolution || torrent.quality;
+    render() {
+        let torrents = this.props.torrents
+            ? this
+                .props
+                .torrents
+                .map((torrent) => {
+                    let title = torrent.title;
+                    let videoQuality = torrent.resolution || torrent.quality;
 
-					return (
-						<Torrent
-							key={uniqid()}
-							torrent={torrent}
-							name={`torrent ${
-								this.props.getCurrentMagnet() == torrent.magnet
-									? "active"
-									: ""
-							}`}
-							videoQuality={videoQuality}
-							title={title}
-							handleTorrentClick={this.props.handleTorrentClick}
-						/>
-					);
-			  })
-			: "";
-		return (
-			<div className='backup-container'>
-				<div className='title'>Torrents</div>
-				<div className='subtitle'>
-					{this.props.torrents
-						? `Select from one of the alternative torrents for "${
-								this.props.movie.title
-						  }" below.`
-						: "We couldn't find any alternative torrents. Please wait or try again."}
-				</div>
-				{this.props.torrents ? (
-					<div className='torrent-container'>{torrents}</div>
-				) : (
-					false
-				)}
-				<div className='reload-btn' onClick={this.handleReload}>
-					<span>Reload</span>
-				</div>
-			</div>
-		);
-	}
+                    return (<Torrent
+                        key={uniqid()}
+                        torrent={torrent}
+                        name={`torrent ${this
+                        .props
+                        .getCurrentMagnet() == torrent.magnet
+                        ? "active"
+                        : ""}`}
+                        videoQuality={videoQuality}
+                        title={title}
+                        handleTorrentClick={this.props.handleTorrentClick}/>);
+                })
+            : "";
+        return (
+            <div className='backup-container'>
+                <div className='title'>Torrents</div>
+                <div className='subtitle'>
+                    {this.props.torrents
+                        ? `Select from one of the alternative torrents for "${
+                    this.props.movie.title}" below.` : "We couldn't find any alternative torrents. Please wait or try again."}
+                </div>
+                {this.props.torrents
+                    ? (
+                        <div className='torrent-container'>{torrents}</div>
+                    )
+                    : (false)}
+                <div className='reload-btn' onClick={this.handleReload}>
+                    <span>Reload</span>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default BackupTorrents;
