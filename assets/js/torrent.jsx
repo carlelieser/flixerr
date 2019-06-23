1012a898adf53d9ae22789b20f933e21e83f6bbe
Carlos Santos <carloseliesersantos@gmail.com>
2019-06-22 20:44:34 -0400

    Update torrent.jsx

--- assets/js/torrent.jsx
+++ assets/js/torrent.jsx
@@ -1,32 +1,34 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import Fade from "react-reveal/Fade";
 
 class Torrent extends Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	handleTorrent = () => {
-		this.props.handleTorrentClick(this.props.torrent);
-	};
+    handleTorrent = () => {
+        this
+            .props
+            .handleTorrentClick(this.props.torrent);
+    };
 
-	render() {
-		return (
-			<Fade distance='10%' bottom>
-				<div className={this.props.name} onClick={this.handleTorrent}>
-					{this.props.videoQuality ? (
-						<span className='video-quality'>
-							{this.props.videoQuality}
-						</span>
-					) : (
-						""
-					)}
-					<span className='title'>{this.props.title}</span>
-				</div>
-			</Fade>
-		);
-	}
+    render() {
+        return (
+            <Fade distance='10%' bottom>
+                <div className={this.props.name} onClick={this.handleTorrent}>
+                    {this.props.videoQuality
+                        ? (
+                            <span className='video-quality'>
+                                {this.props.videoQuality}
+                            </span>
+                        )
+                        : ("")}
+                    <span className='title'>{this.props.title}</span>
+                </div>
+            </Fade>
+        );
+    }
 }
 
 export default Torrent;
