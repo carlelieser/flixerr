2255c239d89573ef33c32c06484a65df2dc8fd33
Carlos Santos <carloseliesersantos@gmail.com>
2019-06-22 20:43:37 -0400

    Update search-container.jsx

--- assets/js/search-container.jsx
+++ assets/js/search-container.jsx
@@ -1,56 +1,65 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import uniqid from "uniqid";
 
 import MovieItem from "./movie-item";
 
 class SearchContainer extends Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		if (nextProps.searchContent === this.props.searchContent) {
-			return false;
-		} else {
-			return true;
-		}
-	}
+    setHeader = () => {
+        if (this.props.searchContent) {
+            if (this.props.searchContent.length) {
+                this
+                    .props
+                    .setHeader(this.props.searchContent[0].flixerr_data.backdrop_path);
+            }
+        }
+    }
 
-	componentDidUpdate(prevProps, prevState) {
-		if (prevProps.searchContent !== this.props.searchContent) {
-			this.props.setHeader(this.props.searchContent);
-		}
-	}
-	componentDidMount() {
-		this.props.setHeader(this.props.searchContent);
-	}
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.searchContent === this.props.searchContent) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-	render() {
-		let searchContent = this.props.searchContent.$$typeof
-			? this.props.searchContent
-			: this.props.searchContent.map((movie) => (
-					<MovieItem
-						key={uniqid()}
-						movie={movie}
-						fallback={true}
-						openBox={this.props.openBox}
-					/>
-			  ));
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.searchContent !== this.props.searchContent) {
+            this.setHeader();
+        }
+    }
+    componentDidMount() {
+        this.setHeader();
+    }
 
-		return (
-			<div className='search-container'>
-				{this.props.searchContent.$$typeof ? (
-					""
-				) : (
-					<div className='search-title'>{`Search Results (${
-						this.props.searchContent.length
-					})`}</div>
-				)}
-				<div className='search-results'>{searchContent}</div>
-			</div>
-		);
-	}
+    render() {
+        let searchContent = this.props.searchContent.$$typeof
+            ? this.props.searchContent
+            : this
+                .props
+                .searchContent
+                .map((movie) => (<MovieItem
+                    key={uniqid()}
+                    movie={movie}
+                    fallback={true}
+                    openBox={this.props.openBox}/>));
+
+        return (
+            <div className='search-container'>
+                {this.props.searchContent.$$typeof
+                    ? ("")
+                    : (
+                        <div className='search-title'>{`Search Results (${
+                            this.props.searchContent.length})`}</div>
+                    )}
+                <div className='search-results'>{searchContent}</div>
+            </div>
+        );
+    }
 }
 
 export default SearchContainer;
