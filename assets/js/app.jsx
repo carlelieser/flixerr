a3b5a6f7cc2a45314dbbfad2d653fd7f3e6e28b9
Skyfall <49743259+RadGade@users.noreply.github.com>
2020-05-07 14:04:49 +0930

    even the other one went down so here is a new one

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1384,7 +1384,7 @@ class App extends Component {
     fetchFromPopcorn = (id, movie) => {
         let isSeries = this.isSeries(movie),
             type = isSeries ? 'show' : 'movie',
-            url = `http://popcorn-api.io/${type}/${id}`
+            url = `https://movies-v2.api-fetch.sh/${type}/${id}`
 
         return this.fetchContent(url)
             .then((data) => {
