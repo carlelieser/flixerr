28770e08d6ab91156ea1005e46c625b683e37ee7
Carlos Santos <carloseliesersantos@gmail.com>
2020-09-12 00:50:23 -0400

    update subtitles host

--- assets/js/subtitles/subtitle-search.js
+++ assets/js/subtitles/subtitle-search.js
@@ -6,8 +6,8 @@ let SubtitleSearch = function () {
 
     let provider = {
         name: 'YIFY-Subtitles',
-        baseURL: 'https://www.yifysubtitles.com',
-        url: `https://www.yifysubtitles.com/search?q=`,
+        baseURL: 'https://www.yifysubtitles.org',
+        url: `https://www.yifysubtitles.org/search?q=`,
         queryFunction: (query) => {
             return encodeURI(query)
         },
@@ -20,79 +20,67 @@ let SubtitleSearch = function () {
 
         let list = $('.other-subs').children('tbody').children()
         let subs = []
-
         for (let i = 0; i < langs.length; i++) {
             let lang = langs[i]
-            let subList = list.filter((index) => {
-                let element = $(list[index])
-                let subLang = element.find('.sub-lang').text()
-                let isLang = subLang == lang
-
-                return isLang
-            })
-
-            let subURL = $(subList[0]).find('.download-cell>a').attr('href')
+            let primaryLink = list
+                .find('a')
+                .toArray()
+                .find((element) => {
+                    let link = $(element)
+                    let isSubLink =
+                        link.attr('href').startsWith('/subtitles') &&
+                        link
+                            .attr('href')
+                            .toLowerCase()
+                            .indexOf(lang.toLowerCase()) > -1
+                    return isSubLink
+                })
+            let subURL = primaryLink ? $(primaryLink).attr('href') : null
+            let subDownloadURL = subURL
+                ? `${subURL.replace('subtitles', 'subtitle')}.zip`
+                : null
 
             let sub = {
                 lang,
-                url: `${provider.baseURL}${subURL}`,
+                url: `${provider.baseURL}${subDownloadURL}`,
             }
-
-            if (subURL) {
-                subs.push(sub)
-            }
+            if (subURL) subs.push(sub)
         }
 
         return subs
     }
 
-    let downloadSubtitle = (url) => {
-        return request.get(url).then((response) => {
-            let download = require('download')
+    let downloadSubtitle = (metadata) => {
+        let { lang, url } = metadata
+        let download = require('download')
 
-            let body = response.data
-            let $ = cheerio.load(body)
+        return download(url).then((data) => {
+            let decompress = require('decompress')
+            let detect = require('charset-detector')
 
-            let subLink = $('.download-subtitle').attr('href')
-            let lang = $('.movie-main-info')
-                .children('.row')
-                .children('.col-xs-10')
-                .children('.list-group')
-                .children('.list-group-item')
-                .first()
-                .children('span')
-                .first()
-                .text()
-                .trim()
-
-            return download(subLink).then((data) => {
-                let decompress = require('decompress')
-                let detect = require('charset-detector')
-
-                return decompress(data).then((files) => {
-                    let encoding = require('encoding')
-                    let srt = files.find((file) => {
-                        return (
-                            file.path.indexOf('.srt') > -1 &&
-                            file.path.indexOf('MACOSX') === -1
-                        )
-                    })
-
-                    if (srt) {
-                        let data = srt.data
-                        let charset = detect(data)[0].charsetName
-                        let converted = encoding.convert(data, 'utf-8', charset)
-
-                        let file = {
-                            data: converted,
-                            name: `${lang}.srt`,
-                            lang,
-                        }
-                        return file
-                    }
-
-                    return srt
+            return decompress(data).then((files) => {
+                let encoding = require('encoding')
+                let srt = files.find((file) => {
+                    return (
+                        file.path.indexOf('.srt') > -1 &&
+                        file.path.indexOf('MACOSX') === -1
+                    )
                 })
+
+                if (srt) {
+                    let data = srt.data
+                    let charset = detect(data)[0].charsetName
+                    let converted = encoding.convert(data, 'utf-8', charset)
+
+                    let file = {
+                        data: converted,
+                        name: `${lang}.srt`,
+                        lang,
+                    }
+                    return file
+                }
+
+                return srt
             })
         })
     }
@@ -103,7 +91,7 @@ let SubtitleSearch = function () {
 
         for (let i = subs.length - 1; i >= 0; i--) {
             let sub = subs[i]
-            let promise = downloadSubtitle(sub.url)
+            let promise = downloadSubtitle(sub)
             promises.push(promise)
         }
 
@@ -137,7 +125,8 @@ let SubtitleSearch = function () {
     }
 
     let search = (movieName, movieYear) => {
-        let url = `${provider.url}${provider.queryFunction(movieName)}`,
+        let encodedQuery = provider.queryFunction(movieName)
+        let url = `${provider.url}${encodedQuery}`,
             year = movieYear ? movieYear.substring(0, 4) : false
 
         return request.get(url).then((response) => {
@@ -148,7 +137,6 @@ let SubtitleSearch = function () {
 
                 let imdb = getIMDB(movieName, year, body),
                     movieURL = `${provider.baseURL}${imdb}`
-
                 return request.get(movieURL).then((response) => {
                     let data = response.data
                     let subtitles = getSubtitlesFromData(data)
